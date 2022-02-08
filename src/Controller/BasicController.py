91e179a4304a261b472398ed21fe816bc1da5a59
tianhao wu <shogun2015@hotmail.com>
2022-02-09 04:21:21 +0800

    get veh info done

--- src/Controller/BasicController.py
+++ src/Controller/BasicController.py
@@ -5,7 +5,42 @@ from enum import Enum
 import traci
 from src.const import const_var
 
+# the vehicle number on junction lane
+veh_num_in_Junction = {
+    ":J_C_0_0": 0,
+    ":J_C_1_0": 0,
+    ":J_C_2_0": 0,
+    ":J_C_3_0": 0,
+    ":J_C_4_0": 0,
+    ":J_C_5_0": 0,
+    ":J_C_6_0": 0,
+    ":J_C_7_0": 0
+}
 
+# a vehicle info list in dict
+# item in vehicle info list [id, type, pos]
+vehs_in_lane = {
+    "u_0": [],
+    "u_1": [],
+    "r_0": [],
+    "r_1": [],
+    "d_0": [],
+    "d_1": [],
+    "l_0": [],
+    "l_1": []
+}
+
+# a dict contains each lane's first ICV
+first_ICV_in_lane = {
+    "u_0": [],
+    "u_1": [],
+    "r_0": [],
+    "r_1": [],
+    "d_0": [],
+    "d_1": [],
+    "l_0": [],
+    "l_1": []
+}
 
 
 class LaneType(Enum):
@@ -37,62 +72,78 @@ class BasicController:
         self.ignore_rightTurn = False
         self.vehicles_info = dict()  # {vehicleID, [laneID, LaneType, LanePos]}
         self.travelTimeDict = dict()  # {vehicleID, {enterTime, exitTime}}
-        self.collisionVehPair = []  # [[veh1id, veh2id],...]
+        # self.collisionVehPair = []  # [[veh1id, veh2id],...]
         self.time_step = 0
         self.numCollision = -1
         self.in_loop = ['ui_0', 'ui_1', 'di_0', 'di_1', 'ri_0', 'ri_1', 'li_0', 'li_1']
         self.exit_loop = ['uo_0', 'uo_1', 'do_0', 'do_1', 'ro_0', 'ro_1', 'lo_0', 'lo_1']
 
-        # get info from const.py
-        self.LANE_ID_set = const_var.LANE_ID
-        self.junction_sourceLane_map = const_var.junction_sourceLane_map
-        self.Junction_ID_set = const_var.JUNCTION_ID
-
-        # config the controller
-        self.setIgnoreTurnRight(True)
-
-    def setIgnoreTurnRight(self, option):
-        if option:
-            self.ignore_rightTurn = True
-            self.LANE_ID_set = list(set(const_var.LANE_ID) - {"l_0", "b_0", "r_0", "u_0"})
-
     def __str__(self):
         return "Basic Controller"
 
     def simulation_step(self, time_step):
-        '''
+        """
         control each vehicle's action at a time step
         :param time_step:
         :return:
-        '''
+        """
         self.time_step = time_step
+        self.data_clear_step()
         self._get_vehicles_info()
-        self._collectCollisionNum()
+        # self._collectCollisionNum()
         self._collectTraveTime()
         self.controlVehicle()
 
     def _get_vehicles_info(self):
-        '''
-        Get the vehicles information in Buffered Area
+        """
+        Get vehicles information in Buffered Area
         :return:
-        '''
-        self.vehicles_info.clear()
+        """
         vids = traci.vehicle.getIDList()
 
+        # Produce vehs_in_lane
         for vid in vids:
             veh_color = traci.vehicle.getColor(vid)
+            veh_type = "HDV" if (veh_color == (0, 0, 255, 255)) else "ICV"
             veh_pos = self.getVehPos(vid)
             veh_lane = self.getVehLane(vid)  # [laneID, LaneType]
             veh_lane_ID = veh_lane[0]
             veh_lane_Type = veh_lane[1]
-            veh_lane_pos = self.getVehLanePosition(vid)
+            veh_lane_pos = const_var.LANE_LENGTH - 10 - round(traci.vehicle.getLanePosition(vid))
 
-            if veh_lane_ID in self.LANE_ID_set:
-                maxAllowedSpeed = traci.lane.getMaxSpeed(veh_lane_ID)
-                traci.vehicle.setMaxSpeed(vid, maxAllowedSpeed)
-                self.vehicles_info[vid] = [veh_lane_ID, veh_lane_Type, veh_lane_pos]
+            # Classification by Lane ID
+            if veh_lane_ID in const_var.LANE_ID:
+                if vehs_in_lane[veh_lane_ID]:  # vehicle list not empty
+                    for index, value in enumerate(vehs_in_lane[veh_lane_ID]):
+                        if veh_lane_pos < value[2]:  # the shortest dist to junction first
+                            vehs_in_lane[veh_lane_ID].insert(index, [vid, veh_type, veh_lane_pos])
+                            break
+                        elif index + 1 == len(vehs_in_lane[veh_lane_ID]):
+                            vehs_in_lane[veh_lane_ID].insert(index + 1, [vid, veh_type, veh_lane_pos])
+                            break
+                else:
+                    vehs_in_lane[veh_lane_ID].insert(0, [vid, veh_type, veh_lane_pos])
+            elif veh_lane_ID in const_var.JUNCTION_ID:
+                veh_num_in_Junction[veh_lane_ID] += 1
+            else:
+                # ignore exit lane
+                pass
+
+        # Produce first_ICV_in_lane
+        for LaneID in vehs_in_lane.keys():
+            for index, veh_info in enumerate(vehs_in_lane[LaneID]):
+                if veh_info[1] == "ICV":
+                    first_ICV_in_lane[LaneID].append(
+                        [veh_info[0], veh_info[1], veh_info[2], index])  # [vid, ICV/HDV, dist2junction, front_veh_num]
+
+        print(first_ICV_in_lane)
 
     def getVehPos(self, vid):
+        """
+        Get vehicle vid's position
+        :param vid:
+        :return:
+        """
         L = traci.vehicle.getLength(vid)
         heading = traci.vehicle.getAngle(vid)
         pos = traci.vehicle.getPosition(vid)
@@ -104,10 +155,15 @@ class BasicController:
         return [_x, _y]
 
     def getVehLanePosition(self, vid):
-        if traci.vehicle.getLaneID(vid) in self.LANE_ID_set:
+        """
+        Get vid's position on the corresponding lane.
+        :param vid:
+        :return:
+        """
+        if traci.vehicle.getLaneID(vid) in const_var.LANE_ID:
             # vehicle on entering lane
             return traci.vehicle.getLanePosition(vid)
-        elif traci.vehicle.getLaneID(vid) in self.Junction_ID_set:
+        elif traci.vehicle.getLaneID(vid) in const_var.JUNCTION_ID:
             # vehicle in merging area
             return 200 + traci.vehicle.getLanePosition(vid)
         else:
@@ -120,49 +176,50 @@ class BasicController:
         :param vid: the ID of vehicle
         :return: [LaneID, LaneType]
         """
-        if traci.vehicle.getLaneID(vid) in self.LANE_ID_set:
+        if traci.vehicle.getLaneID(vid) in const_var.LANE_ID:
             # vehicle on entering lane
             return [traci.vehicle.getLaneID(vid), LaneType.Entering]
-        elif traci.vehicle.getLaneID(vid) in self.Junction_ID_set:
+        elif traci.vehicle.getLaneID(vid) in const_var.JUNCTION_ID:
             # vehicle in merging area
-            return [self.junction_sourceLane_map[traci.vehicle.getLaneID(vid)], LaneType.inJunction]
+            # return [self.junction_sourceLane_map[traci.vehicle.getLaneID(vid)], LaneType.inJunction]
+            return [traci.vehicle.getLaneID(vid), LaneType.inJunction]
         else:
             # vehicle on exiting lane
             return [traci.vehicle.getLaneID(vid), LaneType.Exiting]
 
-    def _collectCollisionNum(self):
-        """
-        Record collision vehicle pair
-        :return:
-        """
-        _vids = traci.vehicle.getIDList()
-        # O(n2) traverse all pair inside junction
-        for vid in _vids:
-            veh_lane_type_1 = self.getVehLane(vid)[1]
-            if veh_lane_type_1 is not LaneType.inJunction:
-                continue
-            for vidOther in _vids:
-                veh_lane_type_2 = self.getVehLane(vidOther)[1]
-                if veh_lane_type_2 is not LaneType.inJunction:
-                    continue
-                if vid != vidOther:
-                    veh1Pos = self.getVehPos(vid)
-                    veh2Pos = self.getVehPos(vidOther)
-                    width = traci.vehicle.getWidth(vid)
-                    height = traci.vehicle.getHeight(vid)
-                    angle1 = traci.vehicle.getAngle(vid)
-                    angle2 = traci.vehicle.getAngle(vidOther)
-                    rect1 = ((veh1Pos[0], veh1Pos[1]), (width, height), angle1)
-                    rect2 = ((veh2Pos[0], veh2Pos[1]), (width, height), angle2)
-
-                    if cv2.rotatedRectangleIntersection(rect1, rect2)[0] in [1, 2]:
-                        if [vid, vidOther] not in self.collisionVehPair \
-                                and [vidOther, vid] not in self.collisionVehPair:
-                            self.collisionVehPair.append([vid, vidOther])
-        numCollision_inFunction = len(self.collisionVehPair)
-        if self.numCollision != numCollision_inFunction:
-            print("Timestamp:{} \t The collision num: {}".format(self.time_step, numCollision_inFunction))
-            self.numCollision = numCollision_inFunction
+    # def _collectCollisionNum(self):
+    #     """
+    #     Record collision vehicle pair
+    #     :return:
+    #     """
+    #     _vids = traci.vehicle.getIDList()
+    #     # O(n2) traverse all pair inside junction
+    #     for vid in _vids:
+    #         veh_lane_type_1 = self.getVehLane(vid)[1]
+    #         if veh_lane_type_1 is not LaneType.inJunction:
+    #             continue
+    #         for vidOther in _vids:
+    #             veh_lane_type_2 = self.getVehLane(vidOther)[1]
+    #             if veh_lane_type_2 is not LaneType.inJunction:
+    #                 continue
+    #             if vid != vidOther:
+    #                 veh1Pos = self.getVehPos(vid)
+    #                 veh2Pos = self.getVehPos(vidOther)
+    #                 width = traci.vehicle.getWidth(vid)
+    #                 height = traci.vehicle.getHeight(vid)
+    #                 angle1 = traci.vehicle.getAngle(vid)
+    #                 angle2 = traci.vehicle.getAngle(vidOther)
+    #                 rect1 = ((veh1Pos[0], veh1Pos[1]), (width, height), angle1)
+    #                 rect2 = ((veh2Pos[0], veh2Pos[1]), (width, height), angle2)
+    #
+    #                 if cv2.rotatedRectangleIntersection(rect1, rect2)[0] in [1, 2]:
+    #                     if [vid, vidOther] not in self.collisionVehPair \
+    #                             and [vidOther, vid] not in self.collisionVehPair:
+    #                         self.collisionVehPair.append([vid, vidOther])
+    #     numCollision_inFunction = len(self.collisionVehPair)
+    #     if self.numCollision != numCollision_inFunction:
+    #         print("Timestamp:{} \t The collision num: {}".format(self.time_step, numCollision_inFunction))
+    #         self.numCollision = numCollision_inFunction
 
     def _collectTraveTime(self):
         """
@@ -185,9 +242,18 @@ class BasicController:
                 exit_time = vehData[3]
                 if exit_time != -1.0 and veh_id in list(self.travelTimeDict.keys()):
                     self.travelTimeDict[veh_id].add_exit_time(exit_time)
-                    print("timestep:{} \t vid:{} \t travel_time:{}".format(self.time_step, veh_id,
-                                                                           self.travelTimeDict[veh_id].exit_time -
-                                                                           self.travelTimeDict[veh_id].entry_time))
+                    # print("timestep:{} \t vid:{} \t travel_time:{}".format(self.time_step, veh_id,
+                    #                                                        self.travelTimeDict[veh_id].exit_time -
+                    #                                                        self.travelTimeDict[veh_id].entry_time))
+
+    def data_clear_step(self):
+        self.vehicles_info.clear()
+        for key in veh_num_in_Junction.keys():
+            veh_num_in_Junction[key] = 0
+        for key in vehs_in_lane.keys():
+            vehs_in_lane[key].clear()
+        for key in first_ICV_in_lane.keys():
+            first_ICV_in_lane[key].clear()
 
     def controlVehicle(self):
         """
